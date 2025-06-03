d00968ababf0751b05a23beb572a3ab585a4f71d
SharmarAndrey <sharmarandrey@gmail.com>
2025-06-03 20:20:21 +0200

    MySQL exercicio noticias

---
+++ notocias/conexion.php
@@ -0,0 +1,14 @@
+<?php
+
+// INI
+$host = "localhost";
+$user = "nascor";
+$password = "randomidzeuser";
+$database = "curso_naskor";
+// Conexión
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$database;charset=utf8mb4", $user, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die(" Error de conexión: " . $e->getMessage());
+}
